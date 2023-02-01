bfb1968e84ea5a3c3107337e22c392942926060d
MatteoBax <98881971+MatteoBax@users.noreply.github.com>
2023-02-01 16:52:56 +0100

    aggiunti cruciverba

--- Cruciverba/script/gameData.js
+++ Cruciverba/script/gameData.js
@@ -1,11 +1,27 @@
 //Dettagli cruciverba (Nome, numero caselle)
 const cruciverba=[
     {"Nome":"A","x":5,"y":5},
-    {"Nome":"B","x":4,"y":4}
+    {"Nome":"B","x":4,"y":4},
+    {"Nome":"Esempio","x":13,"y":11}
 ]
 var s="\u9824";
 //Cruciverba A (riga1, riga2....)
 var righeA=[`ABC${s}E`,`FGHIL`,`MNOPQ`,`RSTUV`,`XYKZA`];
 //Cruciverba B (riga1, riga2....)
 var righeB=[`${s}${s}C${s}`,`EFGH`,`${s}${s}M${s}`,`${s}${s}Q${s}`];
+//Cruciverba Esempio
+var righeEsempio=
+[
+    `${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}D${s}`,
+    `${s}${s}${s}DEMOCTRATICA`,
+    `${s}${s}${s}O${s}${s}${s}${s}E${s}${s}R${s}`,
+    `${s}LAVORO${s}P${s}${s}I`,
+    `${s}${s}${s}E${s}${s}${s}${s}${s}U${s}${s}T${s}`,
+    `${s}${s}${s}R${s}${s}${s}${s}${s}B${s}${s}T${s}`,
+    `LIBERTÀ${s}B${s}${s}I${s}`,
+    `${s}${s}${s}${s}${s}${s}${s}${s}L${s}${s}${s}${s}`,
+    `${s}${s}${s}${s}${s}${s}${s}${s}I${s}${s}${s}${s}`,
+    `${s}${s}${s}${s}${s}${s}${s}${s}C${s}${s}${s}${s}`,
+    `${s}${s}${s}${s}${s}${s}${s}${s}A${s}${s}${s}${s}`
+];
 //Altri cruciverba TODO
