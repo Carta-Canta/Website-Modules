f43a8e1d572c37bdfab6cc148ca05f7600503a14
MatteoBax <98881971+MatteoBax@users.noreply.github.com>
2023-02-13 16:42:15 +0100

    Add files via upload
    Directory structure changed and removed the nonsense crossword

---
+++ Cruciverba/script/solutions.js
@@ -0,0 +1,55 @@
+let giusto = true;
+let solutionArray="";
+//Carico l'array delle soluzioni
+function buildArray(type, maxX, maxY){
+    type=type.toUpperCase();
+    solutionArray = {[type]:[{}]};
+    let id="";
+    let i=0;
+    let nomeArray=eval("righe"+type);
+    let contenutoCella="";
+    for(let yC=0; yC<maxY; yC++){
+        contenutoCella = nomeArray[yC];
+        contenutoCella=contenutoCella.split("");
+        for(let xC=0; xC<maxX; xC++){
+            id="x"+xC+",y"+yC;
+            //array[nomeCruciverba][indice]=[{[coordinate]:"contenutoCella"}]
+            solutionArray[type][i]=[{[id]:contenutoCella[xC]}];
+            i++;
+        }
+    }
+}
+
+//Controllo le soluzioni inserite
+function checkSolution(type, maxX, maxY){
+    buildArray(type,maxX,maxY);
+    let id = "";
+    let toCheck = "";
+    let toCheckvalue = "";
+    let i = 0;
+    let result = "";
+    type=type.toUpperCase();
+    for(let k=0; k<maxY; k++){
+        for(let j=0; j<maxX; j++){
+            id="x"+j+",y"+k;
+            //solutionArray[nomeCruciverba][indice a cui leggere][0][id]
+            result = solutionArray[type][i][0][id];
+            toCheck= document.getElementById(`${id}`);
+            toCheckvalue = document.getElementById(`${id}`).value;
+            toCheckvalue = toCheckvalue.toUpperCase();
+            if(toCheckvalue!=result){
+                toCheck.style.borderColor = "red";
+                giusto=false;
+            }else{
+                if(toCheckvalue!=s){
+                    toCheck.style.borderColor = "#2FFF8D"; // verde chiaro
+                }
+                
+            }
+            i++;
+        }
+    }
+    if(giusto){
+        alert("Il cruciverba è giusto");
+    }
+}
